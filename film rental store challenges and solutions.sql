77c72dd11484d96868f30800492b35dd037845c7
Shiva Teja Chary Andhoju <128379454+Shiva-teja-chary-andhoju@users.noreply.github.com>
2023-05-03 21:26:35 +0530

    film-rental-store-challenges.sql

--- film rental store challenges and solutions.sql
+++ film rental store challenges and solutions.sql
@@ -29,7 +29,8 @@ FROM film
 GROUP BY cost_range
 
 
-3.Task: Create a list of the film titles including their title, length and category name ordered descendingly by the length. Filter the results to only the movies in the category 'Drama' or 'Sports'.
+3.Task: Create a list of the film titles including their title, length and category name ordered descendingly by the length.
+Filter the results to only the movies in the category 'Drama' or 'Sports'.
 
 Question: In which category is the longest film and how long is it?
 Answer: Sports and 184
@@ -163,7 +164,8 @@ FROM
 	 GROUP BY DATE(payment_date),weekday) daily
 
 
-11.Task: Create a list of movies - with their length and their replacement cost - that are longer than the average length in each replacement cost group.
+11.Task: Create a list of movies - with their length and their replacement cost -
+that are longer than the average length in each replacement cost group.
 
 Question: Which two movies are the shortest in that list and how long are they?
 Answer: CELEBRITY HORN and SEATTLE EXPECATIONS with 110 minutes
@@ -177,7 +179,8 @@ WHERE length > (SELECT AVG(length) FROM film f2
 ORDER BY length ASC
 
 
-12.Task: Create a list that shows how much the average customer spent in total (customer life-time value) grouped by the different districts.
+12.Task: Create a list that shows how much the average customer spent in 
+total (customer life-time value) grouped by the different districts.
 
 Question: Which district has the highest average customer life-time value?
 Answer: Saint-Denis with an average customer life-time value of 216.54.
@@ -200,7 +203,9 @@ GROUP BY district
 ORDER BY 2 DESC
 
 
-13.Task: Create a list that shows all payments including the payment_id, amount and the film category (name) plus the total amount that was made in this category. Order the results ascendingly by the category (name) and as second order criterion by the payment_id ascendingly.
+13.Task: Create a list that shows all payments including the payment_id, amount and the 
+film category (name) plus the total amount that was made in this category. Order the results 
+ascendingly by the category (name) and as second order criterion by the payment_id ascendingly.
 
 Question: What is the total revenue of the category 'Action' and what is the lowest payment_id in that category 'Action'?
 Answer: Total revenue in the category 'Action' is 4375.85 and the lowest payment_id in that category is 16055.
